c5abc11b591c4c61868e17563256f82e16fba356
liuchuan97 <1826046383@qq.com>
2020-09-10 10:23:38 +0800

    创建Stu项目

---
+++ gitproject01/src/main/java/StuTest.java
@@ -0,0 +1,11 @@
+public class StuTest {
+
+    public void helloStu(){
+
+        System.out.println("hellow Stu");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hellow Stu");
+    }
+}
