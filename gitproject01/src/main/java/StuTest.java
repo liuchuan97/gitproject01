024a14ebfbc6d9d5d09f8515f728accc030e331c
liuchuan97 <1826046383@qq.com>
2020-09-10 11:18:43 +0800

    修改的Stu项目

--- gitproject01/src/main/java/StuTest.java
+++ gitproject01/src/main/java/StuTest.java
@@ -1,11 +1,15 @@
 public class StuTest {
 
-    public void helloStu(){
+    public void helloStu() {
 
         System.out.println("hellow Stu");
     }
 
     public static void main(String[] args) {
         System.out.println("hellow Stu");
+
+        System.out.println("hi Stu");
+
+        System.out.println("hellow git");
     }
 }
